133be56332c68695264b6850dfd858b57fd0c52a
Bryan Vera <brayanandresveragar@gmail.com>
2024-05-19 11:42:28 -0500

    :bug: Bug mapa no visible

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,9 @@
+import map from "../../assets/images/maps.png";
+
 const Map = () => {
   return (
     <a className="w-full  cursor-pointer" target="blank" href="https://www.google.com/maps?ll=-33.387053,-70.530525&z=18&t=m&hl=es-419&gl=CL&mapclient=embed&cid=13709932103716970485">
-      <img className="w-full lg:min-h-[730px] object-cover rounded-2xl" src="/src/assets/images/maps.png" alt="Dirección de consulta" />
+      <img className="w-full lg:min-h-[730px] object-cover rounded-2xl" src={map} alt="Dirección de consulta" />
     </a>
   );
 };
